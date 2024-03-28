82c6f1f900e50e0684747b2ec6c4e58ce64eb424
andrearlotta <aarlotta@os.uniroma3.it>
2024-03-28 14:52:56 +0100

    Mpc Class Augmentation of d-mpc.Mpc

---
+++ src/unity_drone_sim_bridge/MpcClass.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, field
+from typing import List, Dict, Union, Callable,Any
+from casadi import *
+import do_mpc
+
+@dataclass
+class MpcClass(do_mpc.controller.MPC):
+    model: do_mpc.model.Model
+
+    lterm: Callable[[Any], Any] = field(default_factory= (lambda: None))
+    
+    mterm: Callable[[Any], Any] = field(default_factory= (lambda: None))
+
+    bounds_dict: Dict[str, Dict[str, Dict[str, float]]] = field(default_factory=lambda: {
+        '_x': {'x1': {'upper': None, 'lower': None}},
+        '_u': {'u1': {'upper': None, 'lower': None}},
+    })
+
+    scaling: Dict[str, Dict[str,float]]= field(default_factory=lambda: {
+        '_x': {'x1': None},
+    })
+
+    setup_mpc: dict = field(default_factory=lambda:{
+            'n_horizon': 20,
+            't_step': 0.1,
+            'n_robust': 1,
+            'store_full_solution': True,
+    }
+    )
+
+    def __post_init__(self):
+        super().__init__(self.model)
+        self.set_param(**self.setup_mpc)
+        print(self.mterm(self.model))
+        self.set_objective(mterm=self.mterm(self.model), lterm=self.lterm(self.model))
+
+        for state_type, state_vars in self.bounds_dict.items():
+            for state_var, bounds in state_vars.items():
+                for lu, value in bounds.items():
+                    print(lu,state_type,state_var)
+                    if value is not None: self.bounds[lu,state_type,state_var] = value
+
